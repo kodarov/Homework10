d6e201662fdd898f0be71ead0809104e07c8ba32
kodarov_salavat <kodarov@gmail.com>
2023-01-26 21:56:15 +0400

    1st commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+    }
+    public static void task1(){
+        System.out.println("Задание 1");
+    }
+    public static void task2(){
+        System.out.println("Задание 2");
+    }
+    public static void task3(){
+        System.out.println("Задание 3");
+    }
+    public static void task4(){
+        System.out.println("Задание 4");
+    }
+}
