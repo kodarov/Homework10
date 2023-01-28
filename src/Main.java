12b6d833cb885cacdd8d067e2cd2ac77cb57a512
kodarov_salavat <kodarov@gmail.com>
2023-01-28 10:16:04 +0400

    HW10\task01

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,14 @@ public class Main {
     }
     public static void task1(){
         System.out.println("Задание 1");
+        String firstName = "Ivan";
+        String middleName = "";
+        String lastName = "Ivanov";
+        String fullName = lastName+" "+firstName+" "+middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
+        if (firstName.length() == 0 || middleName.length() == 0 || lastName.length() == 0) {
+            throw new RuntimeException("Отсутствуют полные данные");
+        }
     }
     public static void task2(){
         System.out.println("Задание 2");
